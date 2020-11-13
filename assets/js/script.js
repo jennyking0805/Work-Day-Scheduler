3ddc6d47e29d3a37224e7a31b5e1d74216bae3d3
Jenny King <jennyking0805@me.com>
2020-11-13 11:49:10 -0500

    added time string to set up hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,36 @@
+$(document).ready(function () {
+    var todaysDate = moment().format("dddd, MMMM Do, YYYY");
+    $("#currentDay").text(todaysDate);
 
+    var time = [
+        { stringTime: "7:00AM", numTime: 0700},
+        { stringTime: "8:00AM", numTime: 0700},
+        { stringTime: "9:00AM", numTime: 0700},
+        { stringTime: "10:00AM", numTime: 0700},
+        { stringTime: "11:00AM", numTime: 0700},
+        { stringTime: "12:00PM", numTime: 0700},
+        { stringTime: "1:00PM", numTime: 0700},
+        { stringTime: "2:00PM", numTime: 0700},
+        { stringTime: "3:00PM", numTime: 0700},
+        { stringTime: "4:00PM", numTime: 0700},
+        { stringTime: "5:00PM", numTime: 0700},
+        { stringTime: "6:00PM", numTime: 0700}
+    ];
 
-var NowMoment = moment();
-var eDisplayMoment = document.getElementById('currentDay');
-eDisplayMoment.innerHTML = NowMoment.format('dddd, MMMM Do, YYYY');
+    time.map((hour) => {
 
+        var timeRow =$("<div>");
+        timeRow.attribute("class", "row");
 
-var m = moment();
-var currentTime = moment().hour();
-var taskTime = $("taskInput")
+        var timeSpan =$("<span>");
+        timeSpan.attribute("class", "col-2");
+        timeSpan.text(hour.stringTime);
+        timeRow.append(timeSpan);
 
-var dueTimeStyle = function() {
-    if(article.dataset.time === currentTime) {
-        $("#taskInput").addClass("present")
-    } else if(article.dataset.time < currentTime) {
-        $("#taskInput").addClass("past")
-    } else if(article.data.time > currentTime) {
-        $("#taskInput").addClass("future")
-    }
-}
+        var timeDesription = $("<textarea>");
+        for(i = 0, i< 7; i++) {
+            var timeDesription = $("<textarea>");
+            timeDesription.attribute("class", "col description border");
+        } 
+    })
+});
